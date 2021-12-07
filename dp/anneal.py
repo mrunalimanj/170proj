b92c1881733d6db67e4387079bc41d78933c8665
mrunalimanj <mrunali@berkeley.edu>
2021-12-07 11:16:55 -0800

    update dp anneal.py - working but not improving

--- dp/anneal.py
+++ dp/anneal.py
@@ -4,10 +4,9 @@ from solver import decide_profit
 from parse import read_input_file, read_output_file, write_output_file
 import os
 
-TEMP = 60
 
 
-def calc_profit(order, tasks):
+def calc_profit(order, tasks): # 1-indexed
     total_profit = 0
     timestep = 0
     for ig_i in order:
@@ -18,6 +17,13 @@ def calc_profit(order, tasks):
             raise
 
     return total_profit
+
+def calc_time_used(order, tasks): # 0-indexed
+    total_time = 0
+    for ig in order:
+        total_time += tasks[ig].duration
+    return total_time
+
 
 def get_new_order(old_order, i, j):
     order = [i for i in old_order]
@@ -34,10 +40,12 @@ def get_new_order(old_order, i, j):
         if i in order:
             ind_i = order.index(i)
             order[ind_i] = j
+            order.append(i)
             
         else:
             ind_j = order.index(j)
             order[ind_j] = i
+            order.append(j)
         
         return order
 
@@ -45,10 +53,8 @@ def get_new_order(old_order, i, j):
         return order # no point in switching, need to resample 
 
 def validate_order(order, tasks):
-    total_time = 0 
-    for ig in order:
-        total_time += tasks[ig].duration
-    return total_time < 1440
+    total_time = calc_time_used(order, tasks)
+    return total_time <= 1440
     
 
 def anneal(tasks, initial_task_order):
@@ -59,59 +65,64 @@ def anneal(tasks, initial_task_order):
     # tasks_currently_scheduled = len(initial_task_order)
     curr_prof = calc_profit([o + 1 for o in initial_task_order], tasks)
     best_prof = curr_prof
-    print(f"initially i had {curr_prof}")
+    # print(f"initially i had {curr_prof}")
     best_order = [j for j in initial_task_order]
     task_order = [j for j in initial_task_order]
 
-    iters = 0
-    for iter_i in range(1000):
-        #simulated annealing: pick two to swap
+    temp = 200
+    t_current = temp
+    t_min = 30
+    iter_i = 0
+    total_iters = 800
+    curr_time = calc_time_used(initial_task_order, tasks)
+    best_time =  calc_time_used(initial_task_order, tasks)
+
+    while iter_i < total_iters and t_current > t_min:
+        
         i, j = np.random.choice(num_igloos, 2, replace=False)
         i, j = int(i), int(j)
         new_order = get_new_order(task_order, i, j)
-        check = None
-
-        new_prof = calc_profit(new_order, tasks)
+        
+        new_prof = calc_profit([p + 1 for p in new_order], tasks)
         diff = new_prof - curr_prof
+        new_time = calc_time_used(new_order, tasks)
 
-        update_chance = TEMP/(iter_i + 1) # should be high for early iterations, lower over time
-        accept = np.exp(- diff / update_chance)
-        if diff < 0 or np.random.choice(1) < accept:
+        accept = np.exp(diff / t_current)
+        
+        if np.random.uniform(0, 1) < accept and validate_order(new_order, tasks): # or diff < 0?
             curr_prof = new_prof
-            task_order = new_order
+            curr_time = new_time
+            task_order = [o for o in new_order]
 
 
         if not validate_order(new_order, tasks): # total duration cannot exceed 1440.
             continue # need to resample
 
+
         elif new_prof > best_prof and validate_order(new_order, tasks):
             # update best
             best_prof = new_prof
-            best_order = new_order
+            best_time = new_time
+            best_order = [o for o in new_order]
 
-
+        t_current = t_min + (temp - t_min) * (total_iters - iter_i)/total_iters
+        iter_i += 1
 
     return best_order, best_prof
 
-def check_output(tasks, order, profit):
-    time_now = 0
+def check_output(tasks, order, profit): # tasks are now 1-indexed
+    time_now = calc_time_used([o - 1 for o in order], tasks)
     prof = calc_profit(order, tasks)
-    for i in order:
-        ig = tasks[i - 1]
-        time_now += ig.duration
 
     # print(f"I now have {prof}")
     # something up with the profit?
-    if time_now >= 1440:
-        # print(f"time used was {time_now}")
-        while time_now >= 1440:
-            lost_task = order.pop()
-            time_now -= tasks[lost_task - 1].duration
-        #raise Exception(f"profit was expected to be {profit} but was {prof} and time used was {time_now}")
+    if time_now > 1440:
+        raise Exception(f"profit was expected to be {profit} but was {prof} and time used was {time_now}")
     return order    
 
 def main():
-    
+    reverts = 0
+    improvements = 0
     for input_folder in os.listdir('inputs/'):
 
         if not input_folder.startswith('.'):
@@ -135,7 +146,10 @@ def main():
                     
                     if calc_profit(output_fix, tasks) < calc_profit(initial_order, tasks): # double check: if not good, don't bother using
                         output_fix = initial_order
-                        print("had to revert :(")
+                        reverts +=1
+                    else:
+                        print(calc_profit(output_fix, tasks) - calc_profit(initial_order, tasks) > 1)
+                        improvements += 1
 
                     new_output_path = "anneal/outputs/" + input_folder + "/" + file[:-3] + '.out'
                     if not os.path.exists("anneal/outputs/" + input_folder + "/"):
@@ -147,6 +161,8 @@ def main():
                         print(output)
                         print(output_fix)
                         raise
+
+    print(reverts, improvements)
 
 
 
